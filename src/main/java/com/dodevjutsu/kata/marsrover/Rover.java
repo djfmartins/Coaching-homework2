afba001c3353937b7c853f1169fdfb01008bb18b
Duarte Martins <dumartins@ebay.com>
2016-05-02 19:47:55 +0200

    tdd: created method receive and unit test for changing direction

--- src/main/java/com/dodevjutsu/kata/marsrover/Rover.java
+++ src/main/java/com/dodevjutsu/kata/marsrover/Rover.java
@@ -13,6 +13,35 @@ public class Rover {
         this.direction = direction;
     }
 
+    public void receive(String commands)
+    {
+        for (int i = 0; i < commands.length(); i++) {
+            String newCommand = commands.substring(i, i + 1);
+
+            if (newCommand == "l") {
+                if (direction == "N") {
+                    direction = "W";
+                } else if (direction == "S") {
+                    direction = "E";
+                } else if (direction == "W") {
+                    direction = "N";
+                } else {
+                    direction = "S";
+                }
+            } else if (newCommand == "r") {
+                if (direction == "N") {
+                    direction = "E";
+                } else if (direction == "S") {
+                    direction = "W";
+                } else if (direction == "W") {
+                    direction = "S";
+                } else {
+                    direction = "N";
+                }
+            }
+        }
+    }
+
     @Override
     public boolean equals(Object object)
     {
---
+++ src/test/java/com/dodevjutsu/kata/marsrover/RoverRotationTest.java
@@ -0,0 +1,64 @@
+package com.dodevjutsu.kata.marsrover;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RoverRotationTest {
+
+    @Test
+    public void testNorthRotateLeft() {
+        Rover rover = new Rover(0, 0, "N");
+        rover.receive("l");
+        assertEquals(new Rover(0, 0, "W"), rover);
+    }
+
+    @Test
+    public void testNorthRotateRight() {
+        Rover rover = new Rover(0, 0, "N");
+        rover.receive("r");
+        assertEquals(new Rover(0, 0, "E"), rover);
+    }
+
+    @Test
+    public void testSouthRotateLeft() {
+        Rover rover = new Rover(0, 0, "S");
+        rover.receive("l");
+        assertEquals(new Rover(0, 0, "E"), rover);
+    }
+
+    @Test
+    public void testSouthRotateRight() {
+        Rover rover = new Rover(0, 0, "S");
+        rover.receive("r");
+        assertEquals(new Rover(0, 0, "W"), rover);
+    }
+
+    @Test
+    public void testWestRotateLeft() {
+        Rover rover = new Rover(0, 0, "W");
+        rover.receive("l");
+        assertEquals(new Rover(0, 0, "N"), rover);
+    }
+
+    @Test
+    public void testWestRotateRight() {
+        Rover rover = new Rover(0, 0, "W");
+        rover.receive("r");
+        assertEquals(new Rover(0, 0, "S"), rover);
+    }
+
+    @Test
+    public void testEastRotateLeft() {
+        Rover rover = new Rover(0, 0, "E");
+        rover.receive("l");
+        assertEquals(new Rover(0, 0, "S"), rover);
+    }
+
+    @Test
+    public void testEastRotateRight() {
+        Rover rover = new Rover(0, 0, "E");
+        rover.receive("r");
+        assertEquals(new Rover(0, 0, "N"), rover);
+    }
+}
